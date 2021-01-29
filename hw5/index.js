f335dc427c71b97c80192f78752ed65a3d116287
vladyslava <vladyslava.pylypenko@nure.ua>
2021-01-29 19:25:27 +0200

    hw5

--- hw5/index.js
+++ hw5/index.js
@@ -38,4 +38,21 @@ console.log(counterNew(10))
 
 //task3
 
+function getTimeCheck() {
+	let time = 0;
+	return function firstCall(){
+		if (time == 0) {
+			time = new Date().getTime();
+			let firstC = 'Enabled'
+        	return firstC;
+        }else{
+			let callTime = new Date().getTime();
+    		let result = Math.abs(time - callTime) // сбрасываем знак -
+    		time = callTime; //замыкание
+    		return Math.ceil(result / 1000); //чтобы отображалось в секундах
+		}
+	}
+    
+}
 
+const getTime = getTimeCheck();
